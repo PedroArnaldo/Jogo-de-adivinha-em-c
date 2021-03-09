cc5faafdd4eb29dc8ab38c9ea22763854299133d
Pedro Arnaldo <pedromadeira201502@outlook.com>
2021-03-09 11:02:30 -0300

    meu primeiro commit

---
+++ Jogo_de_Adivinha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    
+    printf("***********************************\n");
+    printf("*Bem-vindo ao Jogo de  Adivinhação*\n");
+    printf("***********************************");
+
+    int secretnumber;
+    secretnumber = 42;
+
+    
+    return 0;
+}
