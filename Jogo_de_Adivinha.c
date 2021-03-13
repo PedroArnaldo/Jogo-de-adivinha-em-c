1ab428af490c72521eab32505f9292c4859cd4ad
Pedro Arnaldo <pedromadeira201502@outlook.com>
2021-03-13 09:25:49 -0300

    funcionalidade de decisão adicionada

--- Jogo_de_Adivinha.c
+++ Jogo_de_Adivinha.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <stdbool.h>
 
 
 int main(void)
@@ -7,11 +9,40 @@ int main(void)
     
     printf("***********************************\n");
     printf("*Bem-vindo ao Jogo de  Adivinhação*\n");
-    printf("***********************************");
+    printf("***********************************\n");
 
-    int secretnumber;
+    bool acertou;
+    int secretnumber, chute;
     secretnumber = 42;
 
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+
+    printf("Você chutou o número %d\n", chute);
+
+    acertou = chute == secretnumber? true : false;
+
+    if(acertou)
+    {
+        printf("Parabéns! Você acertou!");
+
+    }
+    else
+    {
+        int maior = chute > secretnumber;
+        if(maior)
+        {
+            printf("Seu chute foi maior do que o núemro secreto!\n");
+        }
+        else
+        {
+            printf("Seu chute foi menor do que o número secreto!\n");
+        }
+        
+    }
     
+
+
+     
     return 0;
 }
